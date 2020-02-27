8cedc1247e4c53099e6a0e7dd00b4d676e8c20d6
a289237642 <a289237642@163.com>
2020-02-27 17:45:14 +0800

    1

--- Go/1. 两数之和.go
+++ Go/1. 两数之和.go
@@ -10,3 +10,16 @@
 因为 nums[0] + nums[1] = 2 + 7 = 9
 所以返回 [0, 1]
 */
+
+func twoSum(nums []int, target int) []int {
+	v := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		dif := target - nums[i]
+		c, ok := v[dif]
+		if ok != false {
+			return []int{c, i}
+		}
+		v[nums[i]] = i
+	}
+	return []int{-1, -1}
+}
