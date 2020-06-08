e9f660f0b01636d9a1d275b13daeb6315c076048
a289237642 <a289237642@163.com>
2020-06-08 16:31:37 +0800

    11_new

--- Python3/11. 盛最多水的容器.py
+++ Python3/11. 盛最多水的容器.py
@@ -35,3 +35,12 @@ class Solution:
                 j -= 1
             res = max(res,s)
         return res
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left, right, maxarea = 0, len(height) - 1, 0
+        while left < right:
+            maxarea = max(maxarea, min(height[right], height[left]) * (right - left))
+            if height[left] < height[right]: left += 1
+            else: right -= 1
+        return maxarea
